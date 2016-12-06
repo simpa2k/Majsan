c3db392e616014625a81f463366637152e9e6b38
Simon Olofsson <s.olofsson@gmail.com>
2016-12-06 13:52:13 +0100

    Implemented base World class for future subclassing.

--- src/world/World.java
+++ src/world/World.java
@@ -2,18 +2,49 @@ package world;
 
 public class World {
 
+    private int lifespan;
+    private String name;
+    private int timestep;
+    private int numSensors;
+    private int numActions;
+
+    private double[] sensors;
+    private double[] actions;
+
+    private double reward;
+
+    public World(Integer lifespan, String name, int numSensors, int numActions) {
+        
+        if (lifespan == null) {
+            this.lifespan = (int) Math.pow(10, 5);
+        } else {
+            this.lifespan = lifespan;
+        }
+
+        this.name = name;
+        this.timestep = -1;
+        this.numSensors = numSensors;
+        this.numActions = numActions;
+
+        sensors = new double[numSensors];
+        actions = new double[numActions];
+
+        reward = 0;
+
+    }
+
     public Step step(double[] actions) {
         
-        // Placeholder
-        return null;
+        timestep += 1;
+        sensors = new double[numSensors]; 
+        reward = 0;
+
+        return new Step(sensors, reward);
 
     }
 
     public boolean isAlive() {
-        
-        // Placeholder
-        return false;
-
+        return timestep < lifespan;
     }
 
 }
