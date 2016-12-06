b92d94c242e7db8c499b1f1d80d92d542cc05199
Simon Olofsson <s.olofsson@gmail.com>
2016-12-06 12:49:40 +0100

    Fixed typo in Brain.senseActLearn() signature.

--- src/brain/Brain.java
+++ src/brain/Brain.java
@@ -2,7 +2,7 @@ package brain;
 
 public class Brain {
 
-    public double[] senseActLearn(double[] sensors, double rweward) {
+    public double[] senseActLearn(double[] sensors, double reward) {
         return new double[] {0.0, 0.0};
     }
 
